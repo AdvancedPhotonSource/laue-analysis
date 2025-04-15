02c2f9f0dd25850a52cbb1ed29053faebe87e132
matthewrdiamond <geomaniac91@gmail.com>
2025-04-15 13:02:26 -0500

    Added files from the Orthros version. Updated imports. Made changes to pyLaueGo.py to accept config dict from memory instead of only file.

---
+++ pipeline/dataclasses/detector.py
@@ -0,0 +1,71 @@
+from xml.etree.ElementTree import Element, SubElement
+from laueindexing.pipeline.dataclasses import dataclass
+
+from laueindexing.pipeline.dataclasses.roi import ROI
+from laueindexing.pipeline.dataclasses.peaksXY import PeaksXY
+
+
+@dataclass
+class Detector:
+    '''
+    Example output:
+    <detector>
+        <inputImage>/data34b/Run2023-1/XEOL223/HAs_long/LAUE1/HAs_long_laue1_1.h5</inputImage>
+        <detectorID>PE1621 723-3335</detectorID>
+        <exposure unit="sec">0.25</exposure>
+        <Nx>2048</Nx>
+        <Ny>2048</Ny>
+        <totalSum>553646000.0</totalSum>
+        <sumAboveThreshold>2500790.0</sumAboveThreshold>
+        <numAboveThreshold>899.0</numAboveThreshold>
+        <cosmicFilter>True</cosmicFilter>
+        <geoFile>/clhome/EPIX34ID/dev/hannah-dev/laue-indexing/pipeline/config/geoN_2023-02-07_19-19-10.xml</geoFile>
+        <ROI startx="0" endx="2047" groupx="1" starty="0" endy="2047" groupy="1"> </ROI>
+        <peaksXY peakProgram="peaksearch" minwidth="0.2825" threshold="250.0" thresholdRatio="-1" maxRfactor="0.5" maxwidth="27.0" maxCentToFit="18.0" boxsize="18" max_number="50" min_separation="40" peakShape="Lorentzian" Npeaks="39" executionTime="0.83">
+            ...
+        </peaksXY>
+    </detector>
+    '''
+
+    inputImage: str = None
+    detectorID: str = None
+    exposure: float = None
+    exposureUnit: str = 'sec'
+    Nx: int = None
+    Ny: int = None
+    totalSum: float = None
+    sumAboveThreshold: float = None
+    numAboveThreshold: float = None
+    cosmicFilter: bool = None
+    geoFile: str = None
+    roi = ROI()
+    peaksXY = PeaksXY()
+
+    def set(self, key, val):
+        floats = ['exposure', 'totalSum', 'sumAboveThreshold', 'numAboveThreshold']
+        if key in self.__dict__.keys():
+            if key in floats:
+                val = float(val)
+            self.__dict__[key] = val
+        elif key in self.roi.__dict__.keys():
+            self.roi.__dict__[key] = val
+        else:
+            self.peaksXY.set(key, val)
+
+    def getXMLElem(self) -> Element:
+        elem = Element("detector")
+        SubElement(elem, "inputImage").text = self.inputImage
+        SubElement(elem, "detectorID").text = self.detectorID
+        exposure = SubElement(elem, 'exposure')
+        exposure.set('unit', self.exposureUnit)
+        exposure.text = str(self.exposure)
+
+        attrs = ['Nx', 'Ny', 'totalSum', 'sumAboveThreshold', 'numAboveThreshold',
+            'cosmicFilter', 'geoFile']
+        for attr in attrs:
+            SubElement(elem, attr).text = str(getattr(self, attr))
+
+        elem.append(self.roi.getXMLElem())
+        elem.append(self.peaksXY.getXMLElem())
+
+        return elem
---
+++ pipeline/dataclasses/step.py
@@ -0,0 +1,111 @@
+from xml.etree.ElementTree import Element, SubElement
+from laueindexing.pipeline.dataclasses import dataclass
+import h5py
+
+from laueindexing.pipeline.dataclasses.detector import Detector
+from laueindexing.pipeline.dataclasses.indexing import Indexing
+
+@dataclass
+class Step:
+    '''
+    Example output:
+    <step xmlns="">
+        <title> </title>
+        <sampleName> </sampleName>
+        <userName>Liu</userName>
+        <beamline>34ID-E</beamline>
+        <scanNum>276800</scanNum>
+        <date>2023-02-17T04:31:43-06:00</date>
+        <beamBad>0</beamBad>
+        <CCDshutter>out</CCDshutter>
+        <lightOn>0</lightOn>
+        <monoMode>white slitted</monoMode>
+        <Xsample>-182.0</Xsample>
+        <Ysample>3469.42</Ysample>
+        <Zsample>1560.23</Zsample>
+        <depth>nan</depth>
+        <energy unit="keV">14.5533</energy>
+        <hutchTemperature>23.65</hutchTemperature>
+        <sampleDistance>0.0</sampleDistance>
+        <detector>
+            ...
+        </detector>
+        <indexing ...>
+            ...
+        </indexing>
+    </step>
+    '''
+
+    xmlns: str = '' #TODO what does this value represent?
+    title: str = ''
+    sampleName: str = ''
+    userName: str = ''
+    beamline: str = '34ID-E'
+    scanNum: int = None
+    dateExposed: str = ''
+    beamBad: int = None
+    CCDshutter: str = ''
+    lightOn: int = None
+    monoMode: str = ''
+    Xsample: float = None
+    Ysample: float = None
+    Zsample: float = None
+    depth: str = 'nan' #default no depth
+    energy: float = None
+    energyUnit: str = 'keV'
+    hutchTemperature: float = None
+    sampleDistance: float = None
+    detector = Detector()
+    indexing: Indexing = None
+
+    def fromH5(self, filename:str):
+        get = lambda f, val : f[val][0].decode('UTF-8')
+        with h5py.File(filename, 'r') as f:
+            self.title = get(f, 'entry1/title') or ' '
+            self.sampleName = get(f, 'entry1/sample/name') or ' '
+            self.detector.detectorID = get(f, 'entry1/detector/ID')
+            self.detector.Nx = f['entry1/detector/Nx'][0]
+            self.detector.Ny = f['entry1/detector/Ny'][0]
+            CCDshutter = int(f['entry1/microDiffraction/CCDshutter'][0])
+            self.CCDshutter = 'out' if CCDshutter else 'in'
+
+    def set(self, key, val):
+        floats = ['Xsample', 'Ysample', 'Zsample', 'energy', 'hutchTemperature', 'sampleDistance']
+        if key in self.__dict__.keys():
+            if key in floats:
+                val = float(val)
+            self.__dict__[key] = val
+        else:
+            self.detector.set(key, val)
+
+    def getXMLElem(self) -> Element:
+        elem = Element("step")
+        elem.set('xmlns', self.xmlns)
+        attrs = ['title', 'sampleName', 'userName', 'beamline',
+            'scanNum']
+        for attr in attrs:
+            sub = Element(attr)
+            sub.text = str(getattr(self, attr))
+            elem.append(sub)
+
+        SubElement(elem, 'date').text = self.dateExposed
+        attrs = ['beamBad', 'CCDshutter', 'lightOn',
+            'monoMode', 'Xsample', 'Ysample', 'Zsample', 'depth']
+        for attr in attrs:
+            sub = Element(attr)
+            sub.text = str(getattr(self, attr))
+            elem.append(sub)
+
+        energy = SubElement(elem, 'energy')
+        energy.set('unit', self.energyUnit)
+        energy.text = str(self.energy)
+        attrs = ['hutchTemperature', 'sampleDistance']
+
+        for attr in attrs:
+            sub = Element(attr)
+            sub.text = str(getattr(self, attr))
+            elem.append(sub)
+        elem.append(self.detector.getXMLElem())
+        elem.append(self.indexing.getXMLElem())
+
+        return elem
