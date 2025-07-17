5f88740e74d9075ae32996242ae8e50a876bbb34
linked-liszt <anoxymoren@gmail.com>
2025-07-17 18:49:53 -0500

    Update C program and compile system to work with current lib versions & env setup

--- src/laueanalysis/reconstruct/source/recon_cpu/source/WireScan.h
+++ src/laueanalysis/reconstruct/source/recon_cpu/source/WireScan.h
@@ -17,35 +17,35 @@
 typedef short unsigned int BOOLEAN;
 
 /* data structures containing information for the wire scan */
-ws_calibration calibration;
-ws_imaging_parameters imaging_parameters;
-ws_image_set image_set;
-ws_user_preferences user_preferences;
+extern ws_calibration calibration;
+extern ws_imaging_parameters imaging_parameters;
+extern ws_image_set image_set;
+extern ws_user_preferences user_preferences;
 
-gsl_matrix * intensity_map;
-gsl_matrix * intensity_norm;
+extern gsl_matrix * intensity_map;
+extern gsl_matrix * intensity_norm;
 
 /* extra data-holders defined for parallel version */
-vvector p_read_buffer[2]; 	// buffer to hold input data for parallel execution
-vvector p_write_buffer[2]; 	// buffer to hold output data for parallel execution
-int p_ibuff;				// index to which buffer to be worked on
-int rows_default;			// limit on # of rows per stripe, default 64
-int	NUM_THREADS;			// default to 1/
+extern vvector p_read_buffer[2]; 	// buffer to hold input data for parallel execution
+extern vvector p_write_buffer[2]; 	// buffer to hold output data for parallel execution
+extern int p_ibuff;				// index to which buffer to be worked on
+extern int rows_default;			// limit on # of rows per stripe, default 64
+extern int	NUM_THREADS;			// default to 1/
 
-struct HDF5_Header in_header;
-//struct HDF5_Header first_header;
-struct HDF5_Header output_header;
-struct geoStructure geoIn;
+extern struct HDF5_Header in_header;
+//extern struct HDF5_Header first_header;
+extern struct HDF5_Header output_header;
+extern struct geoStructure geoIn;
 
-int		verbose;							/* default to 0 */
-float	percent;							/* default to 100 */
-int		cutoff;								/* default to 0 */
-int		AVAILABLE_RAM_MiB;					/* default to 128 */
-int		cosmic;								/* default to 0 */
-int		detNum;								/* detector number, default to 0 */
-char	distortionPath[FILENAME_MAX];		/* full path to the distortion map */
-float	norm_exponent;						/* exponent to use on normalization image */
-float	norm_threshold;						/* threshold to use on normalization image */
-float	norm_rescale;						/* if using norm_exponent, multiply result by this */
+extern int		verbose;							/* default to 0 */
+extern float	percent;							/* default to 100 */
+extern int		cutoff;								/* default to 0 */
+extern int		AVAILABLE_RAM_MiB;					/* default to 128 */
+extern int		cosmic;								/* default to 0 */
+extern int		detNum;								/* detector number, default to 0 */
+extern char	distortionPath[FILENAME_MAX];		/* full path to the distortion map */
+extern float	norm_exponent;						/* exponent to use on normalization image */
+extern float	norm_threshold;						/* threshold to use on normalization image */
+extern float	norm_rescale;						/* if using norm_exponent, multiply result by this */
 
 #endif
